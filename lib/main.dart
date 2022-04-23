3fb4b43e69aff7e6f8eb0bc36fbe41286e5d7696
jrbyt3s <josee.ramirezi@gmail.com>
2022-04-22 23:39:48 -0500

    before final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,21 +18,49 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  AudioCache audioCache = AudioCache();
+
+  void playNote(int number) {
+    audioCache.play('music/note$number.wav');
+  }
+
+  Widget buildKey(int nota, Color valor) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: Container(
+          color: valor,
+          child: TextButton(
+            onPressed: () {
+              playNote(nota);
+              print("Reproduciendo $nota");
+            },
+            child: const Text(""),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Xylophone App"),
+        title: const Text("Xylophone App"),
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: Text("Click"),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          buildKey(1, Colors.redAccent),
+          buildKey(2, Colors.blueAccent),
+          buildKey(3, Colors.greenAccent),
+          buildKey(4, Colors.deepPurpleAccent),
+          buildKey(5, Colors.deepOrangeAccent),
+          buildKey(6, Colors.lightBlue),
+          buildKey(7, Colors.red),
+        ],
       ),
     );
   }
